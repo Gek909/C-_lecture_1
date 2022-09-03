dfabec788cad484fd98d9c4ea4b00572d390f04e
Evgeniy <gek909@mail.ru>
2022-09-03 17:59:58 +0300

    Intro array method added

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 12, 42, 37, 44, 85, 46, 44, 71, 48 };
+
+int n = array.Length;
+int find = 44;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
