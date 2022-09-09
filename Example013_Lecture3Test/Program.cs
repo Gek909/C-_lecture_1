c39a1342ab284d12d7bbdcf2e7b670a134e974e4
Evgeniy <gek909@mail.ru>
2022-09-09 21:48:37 +0300

    3rd lecture Methods types added

---
+++ Example013_Lecture3Test/Program.cs
@@ -0,0 +1,42 @@
+﻿// Сортировка в массиве:
+
+int[] array = { 1, 3, 6, 2, 6, 7, 3, 1, 6, };
+
+// вывод массива на экран:
+// 1 способ:
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int maxPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] > array[maxPosition]) maxPosition = j;
+        }
+
+        int temporary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
+    }
+
+}
+
+
+PrintArray(array);
+SelectionSort(array);
+
+PrintArray(array);
